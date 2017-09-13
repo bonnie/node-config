4f35c804b32132e2efd5d241491f8454b239bc50
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-09-12 18:10:56 -0700

    Move .eslintrc copy to preinstall script. Make script to make new project directory and install all at once.

--- npm-init.js
+++ npm-init.js
@@ -1,29 +1,21 @@
 // adapted from https://nodesource.com/blog/eleven-npm-tricks-that-will-knock-your-wombat-socks-off
-const fs = require('fs');
 
-const ESLINTRC_SOURCE = '/Users/bonnie/.eslintrc'
-const ESLINTRC_DEST = './.eslintrc'
-
-if (!fs.existsSync(ESLINTRC_SOURCE)) {
-  console.error('ERROR: could not find source .eslintrc file: ', ESLINTRC_SOURCE);
-  process.exit()
-}
-
-const data = fs.readFileSync(ESLINTRC_SOURCE, 'utf-8');
-fs.writeFileSync(ESLINTRC_DEST, data);
+const name = require('path').basename(process.cwd())
 
 module.exports = {
+  name,
+  version: '1.0.0',
   author: 'Bonnie Schulkin',
   scripts: {
-    test: 'export NODE_ENV=test; mocha',
+    preinstall: 'cp /Users/bonnie/.eslintrc .',
+    test: 'export NODE_ENV=test && mocha',
   },
   devDependencies: {
+    ajv: '^5.2.2',
     eslint: '^4.6.1',
     'eslint-config-airbnb': '^15.1.0',
     'eslint-plugin-import': '^2.7.0',
     'eslint-plugin-jsx-a11y': '^5.1.1',
     'eslint-plugin-react': '^7.3.0',
-    chai: '^3.5.0',
-    mocha: '^3.1.2',
   },
 }
