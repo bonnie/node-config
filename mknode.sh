519b31c18c56ee6199641a7fb8bf625ffe7fdb55
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2017-09-13 10:56:31 -0700

    Add alias line to bashrc_lines. Update script to account for usage and projects already exist.

--- mknode.sh
+++ mknode.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+  echo ''
+  echo 'USAGE: mknode <project name>'
+  echo '    A new project directory with <project name> will be built'
+  echo '    in the current directory, and default node options will be'
+  echo '    initialized and installed.'
+  echo ''
+  exit
+fi
+
+if [ -d $1 ]; then
+  echo "$1 already exists. Exiting."
+  echo
+  exit
+fi
+
 mkdir $1
 cd $1
 npm init --yes
